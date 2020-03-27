cb2f81b6fbae61afc7087ba65899ad16d2509721
Muhammad Jihad Robbani <jihad.robbani@yahoo.com>
2020-03-27 10:16:27 +0700

    Create function.js

---
+++ function.js
@@ -0,0 +1,25 @@
+// Tugas 1
+function shoutOut(){
+    return 'Halo Function';
+}
+console.log(shoutOut()); // Menampilkan "Halo Function!" di console
+
+// Tugas 2
+function calculateMultiply(nu1, nu2){
+    return nu1*nu2;
+}
+var num1 = 5;
+var num2 = 6;
+var hasilPerkalian = calculateMultiply(num1,num2);
+console.log(hasilPerkalian); // Menampilkan angka 30
+
+// Tugas 3
+function processSentence(nama, umur, alamat, hobi){
+    return`Nama saya ${nama}, umur saya ${umur} tahun, alamat saya di ${alamat}, dan saya punya hobby yaitu ${hobi}`;
+}
+var name = "Agus";
+var age = 30;
+var address = "Jln. Malioboro, Yogjakarta";
+var hobby = "gaming";
+var fullSentence = processSentence(name,age,address,hobby);
+console.log(fullSentence); // Menampilkan "Nama saya Agus, umur saya 30 tahun, alamat saya di Jln. Malioboro, Yogjakarta, dan saya punya hobby yaitu gaming!"
