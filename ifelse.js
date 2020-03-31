0c0be86d8df0310152cd32f6f136497c029773fb
Muhammad Jihad Robbani <jihad.robbani@yahoo.com>
2020-03-31 11:26:02 +0700

    ===

--- ifelse.js
+++ ifelse.js
@@ -1,15 +1,15 @@
 var nama = '';
 var peran ='Ksatria';
 
-if(nama == ''){
+if(nama === ''){
     console.log(`Nama harus diisi!`);
-} else if(peran == 'Ksatria'){
+} else if(peran === 'Ksatria'){
     console.log(`Selamat datang di Dunia Proxytia, ${nama}`);
     console.log(`Halo Ksatria ${nama}, kamu dapat menyerang dengan senjatamu!`);
-} else if(peran == 'Tabib'){
+} else if(peran === 'Tabib'){
     console.log(`Selamat datang di Dunia Proxytia, ${nama}`);
     console.log(`Halo Tabib ${nama}, kamu akan membantu temanmu yang terluka.`);
-} else if(peran == 'Penyihir'){
+} else if(peran === 'Penyihir'){
     console.log(`Selamat datang di Dunia Proxytia, ${nama}`);
     console.log(`Halo Penyihir ${nama}, ciptakan keajaiban yang membantu kemenanganmu!`);
 } else{
