ed041c55294f2eeb2cfc651d59213caf77ae4be4
Muhammad Jihad Robbani <jihad.robbani@yahoo.com>
2020-03-31 11:29:10 +0700

    ===

--- looping.js
+++ looping.js
@@ -31,7 +31,7 @@ for(var i=20; i>=1; i--){
 
 //counter 1
 for(var i=1; i<=100; i++){
-    if(i%2 == 0){
+    if(i%2 === 0){
         console.log('GENAP');
     } else{
         console.log('GANJIL');
@@ -40,7 +40,7 @@ for(var i=1; i<=100; i++){
 
 //counter 2
 for(var i=1; i<=100; i +=2){
-    if(i%3 == 0){
+    if(i%3 === 0){
         console.log(`${i} KELIPATAN 3`);
     }else {
         console.log('');
@@ -49,7 +49,7 @@ for(var i=1; i<=100; i +=2){
 
 //counter 5
 for(var i=1; i<=100; i +=5){
-    if(i%6 == 0){
+    if(i%6 === 0){
         console.log(`${i} KELIPATAN 6`);
     }else {
         console.log('');
@@ -58,7 +58,7 @@ for(var i=1; i<=100; i +=5){
 
 //counter 9
 for(var i=1; i<=100; i +=9){
-    if(i%10 == 0){
+    if(i%10 === 0){
         console.log(`${i} KELIPATAN 10`);
     }else {
         console.log('');
